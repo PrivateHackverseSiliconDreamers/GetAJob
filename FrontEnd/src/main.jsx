8db2e11f89da742f43563cf81cb72eb452cc1d65
Arhychem <ananonymman@hotmail.com>
2024-04-13 13:59:05 +0100

    update

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,9 +1,11 @@
+import { useContext, createContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import SigninPage from "./pages/SigninPage.jsx"
 
 const router = createBrowserRouter([
   {
@@ -15,8 +17,16 @@ const router = createBrowserRouter([
     element:<LoginPage />,
     path:"/login",
     errorElement : <ErrorPage />
-  }
+  },
+  {
+    element:<SigninPage />,
+    path:"/signgin",
+    errorElement : <ErrorPage />
+  },
 ]);
+
+const [user,setUser] = useState("");
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
