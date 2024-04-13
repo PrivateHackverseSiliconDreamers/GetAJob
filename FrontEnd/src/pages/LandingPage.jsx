58a54438b5461151680dd1f322696e79ffbc3d3a
CynthiaAbi03 <119008657+CynthiaAbi03@users.noreply.github.com>
2024-04-13 12:57:42 +0100

    landingPage-done

--- FrontEnd/src/pages/LandingPage.jsx
+++ FrontEnd/src/pages/LandingPage.jsx
@@ -1,48 +1,65 @@
-import "./stylesheet/LandingPage.css";
-import logo from "../assets/images/silicon_logo.svg";
-import heroIcon from "../assets/images/hero-image.svg";
-import { Navigate, useNavigate } from "react-router";
+import './stylesheet/LandingPage.css';
+import logo from '../assets/images/silicon_logo.svg';
+import heroIcon from '../assets/images/hero-image.svg';
+import { Navigate, useNavigate } from 'react-router';
 
 const LandingPage = () => {
   const navigate = useNavigate();
-  const handleNavigate = ()=>{
-    navigate("/login",)
-  }
+  const handleNavigateLogin = () => {
+    navigate('/login');
+  };
 
   return (
-    <div className="container">
-      <div className="landing-header">
-        <div className="landing-silicon-logo">
-          <img src={logo} alt="silicon logo" />
-          <span className="landing-logo-text"> iliconConnect</span>
-        </div>
-        <div className="landing-right"> const handleNavigate = ()=>{
-    const navigate = useNavigate();
-    Navigate("/login",)
-  }
-          <span>Create an account</span>
-          <button className="landing-login-button">Login</button>
+    <div className="landing-container ">
+      <div className="header-container">
+        <div className="landing-header">
+          <div className="landing-silicon-logo">
+            <img
+              src={logo}
+              alt="silicon logo"
+              className="landing-silicon-logo-img"
+            />
+
+            <p className="landing-logo-text">iliconConnect</p>
+          </div>
+          <div className="landing-header-right">
+            <button className="create-acc-button">Create an account</button>
+            <button
+              onClick={handleNavigateLogin}
+              className="landing-login-button"
+            >
+              Login
+            </button>
+          </div>
         </div>
       </div>
-      <div className="landing-body">
-        <div>
-          <h1>
-            SiliconConnect: Empowering{" "}
-            <span className="landing-highlight">Polytechnique Yaoundé's</span> Students
-            and Alumni, One Opportunity at a Time
-          </h1>
-          <p className="landing-body-text">
-            Welcome to <span >SiliconConnect</span>, your
-            gateway to a world of opportunities! Whether you're a student
-            looking for internships or an alumni seeking new career heights, our
-            platform is here to support you. Join us today and unlock a world of
-            possibilities!
-          </p>
-          <button className="landing-start" onClick={handleNavigate}>
-            <span>Get Started</span>
-          </button>
+      <div className="body-container">
+        <div className="landing-body">
+          <div>
+            <h1 className="landing-hero-text">
+              SiliconConnect: Empowering{' '}
+              <span className="landing-highlight">Polytechnique Yaoundé's</span>{' '}
+              Students and Alumni, One Opportunity at a Time.
+            </h1>
+            <p className="landing-body-text">
+              Welcome to <span>SiliconConnect</span>, your gateway to a world of
+              opportunities! Whether you're a student looking for internships or
+              an alumni seeking new career heights, our platform is here to
+              support you. Join us today and unlock a world of possibilities!
+            </p>
+            <div className="get-started-btn-div">
+              <button
+                className="landing-start-btn"
+                onClick={handleNavigateLogin}
+              >
+                Get Started
+              </button>
+            </div>
+          </div>
+          <div className="hero-img-div">
+            <img src={heroIcon} alt="hero-icon" />
+          </div>
         </div>
-        <img src={heroIcon} alt="hero-icon" />
       </div>
     </div>
   );
