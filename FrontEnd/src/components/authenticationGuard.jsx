7fd72b38a6d4ae2821d43b3c7c66adb78bcd31b5
Arhychem <ananonyman@hotmail.com>
2024-04-20 18:27:03 +0100

    add routes

--- FrontEnd/src/components/authenticationGuard.jsx
+++ FrontEnd/src/components/authenticationGuard.jsx
@@ -1,9 +1,9 @@
-import { useAuthentication } from '../context/AuthContext';
-import { Navigate } from 'react-router';
+import { useAuthentication } from "../context/AuthContext";
+import { Navigate, useLocation } from "react-router";
 
 const AuthenticationGuard = ({ Component }) => {
-  /*   const location = useLocation();
-   */ const { isAuthenticated } = useAuthentication();
+  const location = useLocation();
+  const { isAuthenticated } = useAuthentication();
   return isAuthenticated ? <Component /> : <Navigate to="/login" />;
 };
 
---
+++ FrontEnd/src/context/Routes.js
@@ -0,0 +1,9 @@
+const signinURL = "http://localhost:8000/getaJob/users/getPassword";
+const createUserURL ="http://localhost:8000/getaJob/users/create";
+const createJobURL ="http://localhost:8000/getaJob/Job/create";
+const saveJobURL ="http://localhost:8000/getaJob/Job/save";
+const applyJobURL ="http://localhost:8000/getaJob/Job/apply";
+const getAllJobsURL ="http://localhost:8000/getaJob/Job/getAll";
+
+export {signinURL,createUserURL,createJobURL,saveJobURL,applyJobURL,getAllJobsURL};
+
